b7b9ea985d404cca0fa2e4f924fa73ea812f0cf9
Viswanath3891@gmail.com <v.battini@accenture.com>
2026-03-20 11:11:10 +0530

    Added lwc files

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
-
-export default class HelloWorld extends LightningElement {}
+export default class HelloWorld extends LightningElement {
+        greeting = 'World';
+        changeHandler(event) {
+        this.greeting = event.target.value;
+        }
+}
